58e38ee3ef1b4e7ee65a6d825a89df4a9fe7b8f0
Alexandro Trevisan <alexbaixista@gmail.com>
2020-11-08 00:55:22 -0300

    Update AutoLoot v1.0 [Hellfire].lua

--- Macros/AutoLoot v1.0 [Hellfire].lua
+++ Macros/AutoLoot v1.0 [Hellfire].lua
@@ -1,12 +1,17 @@
-local lootBag = {1074171062}
-local gold = 3821
-local bolt = 7163
-local arrow = 3903
-local potion = 3849
-local diamond = 3878
-local userLoot = {gold, bolt, arrow, potion,diamond, 6884, 8031,4030}
-local waitdragdrop = 400
+local lootBag = 1074733081
+               --gold, bolt, arrow, potion
+local loot     = {3821, 7163, 3903, 3849}
+local jewels   = {3864, 3857, 3859, 3878, 3855, 3861, 3862, 3856, 3877} --pe,tu,bd,fr,ba,ec,ds, fe
+local food     = typeConverter("RUD_FUD_BDF_GQE_IQE_ZPE_SQD_OQE_KPE_VQE_TQE_YSD_JQE_YWI_AXI_PQD_QQD_MQE_SPE_RQE_END_RGG_AQD_FUD_ZBG_WLI_QRD_YLI_NRD_PRD_XLI_ACG_IGI_GUD_HND_QSD")
 Ignore = {}
+
+local userLoot = {}
+
+for k,v in pairs(loot) do userLoot[#userLoot+1] = v end
+for k,v in pairs(jewels) do userLoot[#userLoot+1] = v end
+for k,v in pairs(food) do userLoot[#userLoot+1] = v end
+
+local waitdragdrop = 500
 local function IgnoreItems(itemid)
     Ignore[#Ignore+1] = itemid
 end
@@ -15,15 +20,15 @@ function lootFrom(bagid)
     local loot = ScanItems(true,{ContID=bagid, Type=userLoot,Kind=0})
     for i=1, #loot do
         UO.Drag(loot[i].ID, loot[i].Stack)
-        wait(waitdragdrop)
+        wait(50)
         UO.DropC(lootBag)
-        wait(waitdragdrop)
+        wait(50)
     end
 end
+
 function LookForCorpse()
        local corpsetypes = typeConverter("YFM_")
-       local t = ScanItems(true,{Type=corpsetypes,Dist=3,Kind=1},{ID=Ignore})
-       UO.SysMessage("Encontrado "..#t.." corpos")
+       local t = ScanItems(true,{Type=corpsetypes,Dist=2,Kind=1},{ID=Ignore})
        for i=1, #t do
            UO.LObjectID = t[i].ID
            IgnoreItems(t[i].ID)
@@ -33,9 +38,7 @@ function LookForCorpse()
            wait(waitdragdrop+500)
            lootFrom(t[i].ID)
            local bagloot = ScanItems(true,{ContID=t[i].ID, Type={3701}},{ID=Ignore})
-           UO.SysMessage("Encontrado "..#bagloot.." bags")
            for i=1, #bagloot do
-               UO.SysMessage("Abrindo bag de loot")
                UO.LObjectID = bagloot[i].ID
                IgnoreItems(bagloot[i].ID)
                UO.NextCPosX= 656
